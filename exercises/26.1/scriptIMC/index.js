e28309cc474df5bb33b76460b7f4e84bd8ba2a23
richardwellerson <richardwellerson@gmail.com>
2020-06-10 18:45:17 -0300

    Exercicio feito

---
+++ exercises/26.1/scriptIMC/index.js
@@ -0,0 +1,34 @@
+const readline = require('readline-sync');
+
+const weight = readline.questionFloat('Digite seu peso: ');
+const height = readline.questionFloat('Digite sua altura: ');
+const IMC = weight / Math.pow(height, 2);
+
+const thin = IMC < 18.5;
+const normal = IMC >= 18.5 && IMC <= 24.9;
+const overweight = IMC >= 25 && IMC <= 29.9;
+const obesityI = IMC >= 30 && IMC <= 34.9;
+const obesityII = IMC >= 35 && IMC <= 39.9;
+
+if (thin) {
+  console.log(`Seu IMC é ${IMC} e você está magro demais.`);
+  return;
+}
+if (normal) {
+  console.log(`Seu IMC é ${IMC} e você está no seu peso normal.`);
+  return;
+}
+if (overweight) {
+  console.log(`Seu IMC é ${IMC} e você está no acima do peso.`);
+  return;
+}
+if (obesityI) {
+  console.log(`Seu IMC é ${IMC} e você está com Obesidade Grau I.`);
+  return;
+}
+if (obesityII) {
+  console.log(`Seu IMC é ${IMC} e você está com Obesidade Grau II.`);
+  return;
+}
+
+console.log(`Seu IMC é ${IMC} e você está com Obesidade Grau III ou IV.`);
